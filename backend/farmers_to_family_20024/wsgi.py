6f4e0dc11738d4ee9c4427c25ae7b7983b2a2da1
Crowdbotics <team@crowdbotics.com>
2020-09-04 12:14:26 +0000

    Update models

--- backend/farmers_to_family_20024/wsgi.py
+++ backend/farmers_to_family_20024/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farmers_to_family_20024.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "farmers_to_family_20024.settings")
 
 application = get_wsgi_application()
